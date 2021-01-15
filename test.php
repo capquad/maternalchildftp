2fa6123ae93baaab1be269aef79df836d8dcbaa4
capquad <capquadinvestments@gmail.com>
2021-01-15 19:31:35 +0100

    b

--- test.php
+++
@@ -1,8 +0,0 @@
-<?php
-require('./lib/db/db.php');
-require('./lib/db/database.php');
-
-$db = new Database(DBHOST, DBUSER, DBPASS);
-// $db->connect(DBNAME);
-$password = sha1("08162102300");
-$db->insert("staff", ["phone" => "08162102300", "fname" => "Anonymous", "lname" => "Better", "mname" => "Boyfirend", "passwd" => $password]);
